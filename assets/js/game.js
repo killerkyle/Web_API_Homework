91e6cfce0b7d64ae734b07f5db10bb67d24741ef
Kyle Pace <kylelpace@gmail.com>
2020-06-25 12:54:12 -0600

    first commit

---
+++ assets/js/game.js
@@ -0,0 +1,124 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const progressText = document.getElementById("progressText");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+const timerElement = document.getElementById("timerCount");
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+var timerInterval;
+var timerCount = 60;
+
+let questions = [
+  {
+    question: "Where in the human body would you find the Medulla Oblongata?",
+    choice1: "In the Stomach",
+    choice2: "In the Brain",
+    choice3: "In the Foot",
+    choice4: "In the Neck",
+    answer: 2
+  },
+  {
+    question:
+      "Who was the first American to go into space?",
+    choice1: "Yuri Gagarin",
+    choice2: "James Clerk Maxwell",
+    choice3: "Tom Petty",
+    choice4: "Alan Shephard",
+    answer: 4
+  },
+  {
+    question: "What does the Q in IQ stand for?",
+    choice1: "Quantity",
+    choice2: "Quorum",
+    choice3: "Quality",
+    choice4: "Quotient",
+    answer: 4
+  }
+];
+
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  timerInterval = setInterval(timer, 1000);
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
+
+timer = () => {
+  timerCount--;
+  if (timerCount === 0) {
+    clearInterval(timerInterval);
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("end.html");
+  } else {
+    timerElement.textContent = timerCount;
+  }
+};
+
+getNewQuestion = () => {
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    clearInterval(timerInterval);
+    localStorage.setItem("mostRecentScore", score);
+    //go to the end page
+    return window.location.assign("end.html");
+  }
+  questionCounter++;
+  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+  //Update the progress bar
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
+
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
+
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach(choice => {
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    if (classToApply === "correct") {
+      timerCount = timerCount + 10;
+      incrementScore(CORRECT_BONUS);
+    } else {
+      timerCount = timerCount - 10;
+    }
+
+    selectedChoice.parentElement.classList.add(classToApply);
+
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
+
+startGame();
